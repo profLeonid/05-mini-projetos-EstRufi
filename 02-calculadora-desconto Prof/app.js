8534cbd8f6d8a3fde55e7b21b7de0bc4c979e193
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-03-17 15:30:02 -0300

    enviando  um jeito novo de programar e terminando calculadora de média

---
+++ 02-calculadora-desconto Prof/app.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const calcularValorEconomizado = (preco,desconto) => preco * desconto /100
+
+const calcularPrecoFinal = (preco, valorEconomizado) => preco - valorEconomizado
+
+function selecionarCor (desconto){
+    if (desconto <=5)
+        return "desconto1"
+    else if (desconto <= 10)
+        return "desconto2"
+    else
+        return "desconto3"
+}
+
+function limparClases (){
+    document.getElementById("resultado")
+        .classList.remove("desconto1", "desconto2", "desconto3")
+}
+
+function handleClick (){
+    const preco = Number(document.getElementById("preco").value)
+    const desconto = Number(document.getElementById("desconto").value)
+    const resultado = document.getElementById("resultado")
+
+    const valorEconomizado = calcularValorEconomizado(preco,desconto)
+    const precoFinal = calcularPrecoFinal(preco,valorEconomizado)
+    const cor = selecionarCor(desconto)
+
+    resultado.textContent = `${valorEconomizado} - ${precoFinal}`
+    limparClases()
+    resultado.classList.add(cor)
+}
--- 03-calculadora-media/app.js
+++ 03-calculadora-media/app.js
@@ -1,13 +1,35 @@
 "use strict"
 
-function calcularMedia(){
-    const nota1 = document.getElementById("valor1")
-    const nota2 = document.getElementById("valor2")
-    const nota3 = document.getElementById("valor3")
+const calcularMedia = (valor1,valor2,valor3) => (valor1 + valor2 + valor3) / 3
+
+function situacaoAluno (media){
+    if ( media >= 7)
+        return "aprovado"
+    else if (media < 5)
+        return "reprovado"
+    else
+        return "recuperacao"
+}
+
+function texto (media){
+    if ( media >= 7)
+        return "foi Aprovado IUPIIII"
+    else if (media < 5)
+        return "está reprovado, se lascouuu"
+    else
+        return "está de recuperação, vamos tentar novamente"
+}
+
+function handleClick(){
+    const valor1 = Number(document.getElementById("valor1").value)
+    const valor2 = Number(document.getElementById("valor2").value)
+    const valor3 = Number(document.getElementById("valor3").value)
     const resultado = document.getElementById("result")
 
-    const media = (nota1 + nota2 + nota3) /3
+    const media = calcularMedia(valor1,valor2,valor3).toFixed(2)
+    const palavra = texto(media)
+    const cor = situacaoAluno(media)
 
-    // não está mostrando o calculo
-    resultado.textContent(`sua média é ${media}`)
+    resultado.textContent = `A sua média foi de ${media},\n meus parabéns você ${palavra}.`
+    resultado.classList.add(cor)
 }
